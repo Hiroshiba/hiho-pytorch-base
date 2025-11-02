e3c4479962b45b16e92c6871361f332784f91567
Hiroshiba Kazuyuki <kazuyuki_hiroshiba@dwango.co.jp>
2025-11-02 16:55:58 +0000

    fix(test_utils): 訓練データ数を1000から10000に変更

--- tests/test_utils.py
+++ tests/test_utils.py
@@ -24,7 +24,7 @@ def setup_data_and_config(base_config_path: Path, data_dir: UPath) -> Config:
     config.dataset.valid.root_dir = data_dir
 
     root_dir = config.dataset.train.root_dir
-    train_num, valid_num = 1000, 100
+    train_num, valid_num = 10000, 100
     all_stems = list(map(str, range(train_num + valid_num)))
 
     def _setup_data(
